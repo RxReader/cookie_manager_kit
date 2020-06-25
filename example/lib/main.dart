29c965406f61697c92ae168cf38b71c2177b7439
v7lin <v7lin@qq.com>
2020-06-25 16:32:10 +0800

    v2

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -51,7 +51,9 @@ class _HomeState extends State<Home> {
               Cookie cookie = Cookie.fromSetCookieValue(
                   'JSESSIONID=842DE78C987BEE8334F6855A642075D1; Path=/; HttpOnly');
               await CookieManager.saveCookies(
-                  url: _TEST_URL, cookies: <Cookie>[cookie]);
+                url: _TEST_URL,
+                cookies: <Cookie>[cookie],
+              );
               _showTips('保存Cookie', 'cookie: ${cookie.toString()}');
             },
           ),
